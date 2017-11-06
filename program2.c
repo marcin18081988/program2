237f7aad3dc154f5c99337a86b782b8a7cd25c87
majonez007 <maj0nez@poczta.fm>
2017-11-06 18:58:55 +0100

    drugi program

---
+++ program2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	putchar('v'); /* wypisz znak */
+	putchar('\n'); /* znak konca lini */
+	puts("wypisz string / ciag znakow");
+
+	puts("\nAla ma kota, \na kot ma Ale");
+
+	return 0;
+}
